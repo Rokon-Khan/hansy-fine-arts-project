95e6dd4a1de73a96de2f73203d41640d8f8d7481
Md Rokonuzzaman <rokonjust@gmail.com>
2025-01-29 17:10:12 +0600

    Social Media Link Added in Navbar

--- client-side/src/components/TopNavbar.jsx
+++ client-side/src/components/TopNavbar.jsx
@@ -1,25 +1,28 @@
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaPhoneAlt,
-  FaTwitter,
-} from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaPhoneAlt } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io5";
 // import { Link } from "react-router-dom";
 // import logo from "../assets/hansyeaggy-logo.png";
 
 const TopNavbar = () => {
   return (
-    <div className="flex justify-between items-center py-4 px-6 border-b border-gray-200">
+    <div className="max-w-11/2 flex justify-between items-center py-4 px-6 border-b border-gray-200 mx-auto">
       <div className="flex items-center space-x-4">
-        <span className="text-sm text-gray-600">Follow Us:</span>
-        <a href="#" className="text-gray-600 hover:text-gray-800">
-          <FaInstagram className="text-2xl text-[#b09f7b]" />
+        <span className="text-lg font-bold text-slate-800">Follow Us:</span>
+        <a href="http://www.instagram.com/@YaeggyArt" target="_blank">
+          <FaInstagram className="text-2xl text-[#b09f7b] hover:text-[#e9c475]" />
         </a>
-        <a href="#" className="text-gray-600 hover:text-gray-800">
-          <FaFacebookF className="text-2xl text-[#b09f7b]" />
+        <a href="http://www.facebook.com/YaeggyArt" target="_blank">
+          <FaFacebookF className="text-2xl text-[#b09f7b] hover:text-[#e9c475]" />
         </a>
-        <a href="#" className="text-gray-600 hover:text-gray-800">
-          <FaTwitter className="text-2xl text-[#b09f7b]" />
+        <a href="http://www.youtube.com/@YaeggyArt" target="_blank">
+          <IoLogoYoutube className="text-2xl text-[#b09f7b] hover:text-[#e9c475]" />
+        </a>
+        <a
+          href="https://www.tiktok.com/@yaeggyart?_t=ZT-8sMIBRUmk2s&_r=1"
+          target="_blank"
+        >
+          <FaTiktok className="text-2xl text-[#b09f7b] hover:text-[#e9c475]" />
         </a>
       </div>
       <div className="text-center">
@@ -37,8 +40,8 @@ const TopNavbar = () => {
         </Link> */}
       </div>
       <div className="flex items-center space-x-2">
-        <FaPhoneAlt className="text-2xl text-[#b09f7b]" />
-        <span className="text-gray-600">+1 (409) 987-5874</span>
+        <FaPhoneAlt className="text-2xl text-[#b09f7b] hover:text-[#e9c475]" />
+        <span className="text-slate-800 font-bold">+1 (409) 987-5874</span>
       </div>
     </div>
   );
